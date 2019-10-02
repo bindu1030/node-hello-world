ef1cb74228bc4a2c4d04cb240ca3dc6a179f9bf5
Joel Krooswyk <jkrooswyk@gitlab.com>
2019-10-02 18:50:10 +0000

    Update test/test.js

--- test/test.js
+++ test/test.js
@@ -5,6 +5,6 @@ describe('App', function() {
   it('has the default page', function(done) {
     request(app)
       .get('/')
-      .expect(/Welcome to Express/, done);
+      .expect(/This Node app Deployed by GitLab!/, done);
   });
 }); 
