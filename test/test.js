277bdef66faf69420fa635a4b23e54e1103e68e5
Joel Krooswyk <jkrooswyk@gitlab.com>
2019-10-04 17:52:01 +0000

    Update views/index.pug, test/test.js files

--- test/test.js
+++ test/test.js
@@ -5,6 +5,6 @@ describe('App', function() {
   it('has the default page', function(done) {
     request(app)
       .get('/')
-      .expect(/This is a GitLab deployment of Express/, done);
+      .expect(/GitLab deployed Express/, done);
   });
 }); 
